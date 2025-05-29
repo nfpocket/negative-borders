07e58fda2c1073bacd5048f4c8df4dd4685bbb4b
Norman <>
2025-05-29 23:26:32 +0200

    Add TypeScript declaration file for negative-borders module

---
+++ negative.borders.d.ts
@@ -0,0 +1,5 @@
+declare module "negative-borders" {
+  export function computeNegativeBordersStylesForElement(element: HTMLElement): void;
+
+  export function computeNegativeBordersStyles(): void;
+}
